8fd60a39f4f7f115770f980b9eff3df22efe9641
Lecturer CR 1-6 <lecturer16@ucione.local>
2018-02-27 21:26:03 +0100

    Add Config and DB class

--- classes/Helper.php
+++ classes/Helper.php
@@ -4,8 +4,21 @@ class Helper
 {
 	private function __construct(){}
 	
-	public static function getHeader($title, $file)
+	public static function getHeader($title, $file=null)
 	{
+		if($file){
+			return require_once 'includes/layouts/'.$file.'.php';
+		}
 		
+		return false;
+	}
+	
+	public static function getFooter($file=null)
+	{
+		if($file){
+			return require_once 'includes/layouts/'.$file.'.php';
+		}
+		
+		return false;
 	}
 }
--- index.php
+++ index.php
@@ -2,11 +2,31 @@
 
 	require_once 'core/init.php';
 	
+	$db = DB::getInstance()->query('SELECT * FROM users WHERE username = ?', array('perozder'));
+	
+	echo '<pre>';
+	var_dump($db);
+	die();
+	
 	Helper::getHeader('Algebra Auth', 'main-header');
 
 ?>
-
-
-    <h1>Hello, world!</h1>
-
+    <div class="row">
+		<div class="col-xs-12 col-md-8 col-lg-6 col-md-offset-2 col-lg-offset-3">
+			<div class="jumbotron">
+				<div class="container">
+					<h1>Algebra Auth</h1>
+					<p>Lorem ipsum dolor sit amet!</p>
+					<p>
+						<a class="btn btn-primary btn-lg" href="login.php" role="button">Sign In</a>
+						or
+						<a class="btn btn-primary btn-lg" href="register.php" role="button">Create an account</a> 
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php
+	Helper::getFooter('main-footer');
+?>
     
